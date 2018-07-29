73d973eaf93e695c1561b61e83bc0a14702f605b
mikgor <xxminosxx1@interia.eu>
2018-07-29 18:31:39 +0200

    Delete views

--- Todo/urls.py
+++ Todo/urls.py
@@ -7,5 +7,8 @@ from django.conf.urls import url
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     url(r'^(?P<pk>[0-9]+)/tgupdate/$', views.TaskGroupUpdate.as_view(), name='TaskGroupUpdate'),
+    url(r'^(?P<pk>[0-9]+)/tupdate/$', views.TaskUpdate.as_view(), name='TaskUpdate'),
+    url(r'^(?P<pk>[0-9]+)/tgdelete/$', views.TaskGroupDelete.as_view(), name='TaskGroupDelete'),
+    url(r'^(?P<pk>[0-9]+)/tdelete/$', views.TaskDelete.as_view(), name='TaskDelete'),
     path('admin/', admin.site.urls),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,5 +1,5 @@
 from django.views import generic
-from django.views.generic.edit import UpdateView
+from django.views.generic.edit import UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Task, TaskGroup
 
@@ -12,6 +12,20 @@ class IndexView(generic.ListView):
 
 class TaskGroupUpdate(generic.UpdateView):
     model = TaskGroup
-    fields = ['name']
+    fields = ['name', 'colour']
     template_name_suffix = '_update_form'
+    success_url = reverse_lazy('index')
+
+class TaskUpdate(generic.UpdateView):
+    model = Task
+    fields = ['name', 'details', 'priority', 'deadline', 'is_done', 'priority']
+    template_name_suffix = '_update_form'
+    success_url = reverse_lazy('index')
+
+class TaskGroupDelete(DeleteView):
+    model = TaskGroup
+    success_url = reverse_lazy('index')
+
+class TaskDelete(DeleteView):
+    model = Task
     success_url = reverse_lazy('index')
