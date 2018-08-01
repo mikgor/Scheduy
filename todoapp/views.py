88edfe3a30cc5d1683c493f4bc1f537ee2364a5e
mikgor <xxminosxx1@interia.eu>
2018-08-01 22:12:54 +0200

    Add create views

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,25 +1,40 @@
 from django.views import generic
-from django.views.generic.edit import UpdateView, DeleteView
+from django.views.generic.edit import UpdateView, CreateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Task, TaskGroup
+from .forms import *
 
 class IndexView(generic.ListView):
     template_name = 'todoapp/index.html'
-    context_object_name = 'taskgroup_list'
+    context_object_name = 'IndexList'
+    queryset = Task.objects.all()
 
-    def get_queryset(self):
-        return TaskGroup.objects.all()
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['task_list'] = Task.objects.all()
+        context['taskgroup_list'] = TaskGroup.objects.all()
+        return context
 
-class TaskGroupUpdate(generic.UpdateView):
+class TaskCreate(CreateView):
+    form_class = TaskCreateUpdateForm
+    template_name = 'todoapp/task_form.html'
+    success_url = reverse_lazy('index')
+
+class TaskGroupCreate(CreateView):
+    form_class = TaskGroupCreateUpdateForm
+    template_name = 'todoapp/taskgroup_form.html'
+    success_url = reverse_lazy('index')
+
+class TaskGroupUpdate(UpdateView):
     model = TaskGroup
-    fields = ['name', 'colour']
+    form_class = TaskGroupCreateUpdateForm
     template_name_suffix = '_update_form'
     success_url = reverse_lazy('index')
 
-class TaskUpdate(generic.UpdateView):
+class TaskUpdate(UpdateView):
     model = Task
-    fields = ['name', 'details', 'priority', 'deadline', 'is_done', 'priority']
-    template_name_suffix = '_update_form'
+    form_class = TaskCreateUpdateForm
+    template_name = 'todoapp/task_update_form.html'
     success_url = reverse_lazy('index')
 
 class TaskGroupDelete(DeleteView):
