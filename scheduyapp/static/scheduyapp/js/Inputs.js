25f0cbe903bd60cae7d8b723db185faa40e9b278
mikgor <mikgorzny@gmail.com>
2019-07-27 22:58:26 +0200

    Add date pickers to task form and fix translation

--- scheduyapp/static/scheduyapp/js/Inputs.js
+++ scheduyapp/static/scheduyapp/js/Inputs.js
@@ -71,3 +71,19 @@ function hideTimeInputsAndClear(e) {
    document.getElementById("add"+e+"Btn").style.display = "inline-block";
    document.getElementById("id_"+e).value = "";
 }
+
+function replaceDate(offset, input) {
+   $date = new Date();
+   $date.setDate($date.getDate()+offset);
+   $month = 0;
+   $day = 0;
+   if (($date.getMonth()+1) < 10)
+      $month = '0' + ($date.getMonth() + 1);
+   else
+      $month = $date.getMonth() + 1;
+   if ($date.getDate() < 10)
+      $day = '0' + $date.getDate();
+   else
+      $day = $date.getDate();
+   document.getElementById(input).value = $date.getFullYear() + '-' + $month + '-' + $day;
+}
